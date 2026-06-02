60d39784932e28b7fd7bee8a801bbb40ea8d6467
anirban goswami <agoswami@cineverse.com>
2026-06-02 15:14:07 +0530

    first

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
-model = SentenceTransformer("")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
 class EmbeddingRequest(BaseModel):
